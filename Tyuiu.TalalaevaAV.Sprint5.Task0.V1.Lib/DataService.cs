4f7cc1959d9306be8c7be66be974eecd1fa1a250
Tyuiu.TalalaevaAV@outlook.com <Tyuiu.TalalaevaAV@outlook.com>
2024-11-24 19:36:14 +0500

    Task0

--- Tyuiu.TalalaevaAV.Sprint5.Task0.V1.Lib/DataService.cs
+++ Tyuiu.TalalaevaAV.Sprint5.Task0.V1.Lib/DataService.cs
@@ -7,11 +7,11 @@ namespace Tyuiu.TalalaevaAV.Sprint5.Task0.V1.Lib
         public string SaveToFileTextData(int x)
         {
             string path = Directory.GetCurrentDirectory();
-            string path_new = Path.Combine("app", path, "OutPutFileTask0.txt");
+            string path_new = Path.Combine(path, "OutPutFileTask0.txt");
             double f = x * x * x + 2 * x * x + 5 * x + 4;
             f=Math.Round(f,3);
             File.WriteAllText(path, Convert.ToString(f));
-            return path_new;
+            return Convert.ToString(f);
             
             
         }
