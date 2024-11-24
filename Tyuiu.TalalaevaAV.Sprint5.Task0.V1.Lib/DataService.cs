efecb300a98b2c5363890e6ac4932b31ec6069db
Tyuiu.TalalaevaAV@outlook.com <Tyuiu.TalalaevaAV@outlook.com>
2024-11-24 19:24:59 +0500

    Task00

--- Tyuiu.TalalaevaAV.Sprint5.Task0.V1.Lib/DataService.cs
+++ Tyuiu.TalalaevaAV.Sprint5.Task0.V1.Lib/DataService.cs
@@ -9,8 +9,9 @@ namespace Tyuiu.TalalaevaAV.Sprint5.Task0.V1.Lib
             string path=$@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
             double f = x * x * x + 2 * x * x + 5 * x + 4;
             f=Math.Round(f,3);
+            string path_new = @"/app\OutPutFileTask0.txt";
             File.WriteAllText(path, Convert.ToString(f));
-            return path;
+            return path_new;
             
             
         }
