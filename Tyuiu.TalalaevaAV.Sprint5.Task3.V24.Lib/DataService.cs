3827a81b05bb276a9c90947117dc92ef487d6e5c
Tyuiu.TalalaevaAV@outlook.com <Tyuiu.TalalaevaAV@outlook.com>
2024-11-27 14:37:08 +0500

    Task3

--- Tyuiu.TalalaevaAV.Sprint5.Task3.V24.Lib/DataService.cs
+++ Tyuiu.TalalaevaAV.Sprint5.Task3.V24.Lib/DataService.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.TalalaevaAV.Sprint5.Task3.V24.Lib
             string path = Path.GetTempPath();
             string path_new = Path.Combine(path, "OutPutFileTask3.bin");
             double y = 6.1 * Math.Pow(x, 3) + 0.23*Math.Pow(x,2) + 1.04 * x;
-            y = Math.Pow(y, 3);
+            y = Math.Round(y, 3);
             using (BinaryWriter writer = new BinaryWriter(File.Open(path_new, FileMode.OpenOrCreate), Encoding.UTF8)) 
             {
                 writer.Write(BitConverter.GetBytes(y));
